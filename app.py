acc30e7022f6e702c2419911486e8e4c6077a822
AntuBoccalandro <codehu3@gmail.com>
2024-07-02 10:05:54 -0300

    First Commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import json
+import random
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    random_quote = get_random_quote()
+    return render_template('home.html', random_quote=random_quote)
+
+
+def get_random_quote():
+    quotes = load_quotes()
+    random_key = random.choice(list(quotes['all_quotes'].keys()))
+    return quotes['all_quotes'][random_key]
+    
+
+def load_quotes():
+    with open("quotes.json", "r", encoding="utf-8") as json_file:
+        quotes = json.load(json_file)
+    return quotes
+        
+if __name__ == "__main__":
+    app.run(debug=True)
+    
